f8a36859c8152bf3f49249543161160953b132db
katsuhide <AirMyac@AirMyac.local>
2013-10-13 18:14:17 +0900

    検索結果をjsonにして出力

---
+++ mikan.rb
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+require 'twitter'
+require 'yaml'
+
+## 設定の読み込み
+config = YAML.load_file("config.yml")
+config_p = config["production"]
+
+tw = Twitter::Client.new(
+  consumer_key: config_p["consumer_key"],
+  consumer_secret: config_p["consumer_secret"],
+  oauth_token: config_p["oauth_token"],
+  oauth_token_secret: config_p["oauth_token_secret"]
+)
+
+## search tweets
+def search_tweet(tw, keyword, since_id, fetch_size)
+	result_map  = tw.search(keyword, :count => fetch_size, :result_type => "recent", :since_id => since_id).results.reverse.map 
+      return result_map 
+end
+
+## print result of the search tweets
+def print_tweets(result_map)
+	result_map.each{ |result| 
+		# result  => hash
+		h = Hash::new
+		h.store("id", result.id)
+		h.store("time", result.created_at)
+		h.store("user", "@" + result.from_user)
+		h.store("text", result.text)
+		# hash => json
+		j = JSON.pretty_generate(h)
+		#  json => text file
+		File.open('json.txt', 'a'){|file|
+			file.write(j)
+			file.write("\n")
+		}	
+	}
+end
+
+## データの検索
+since_id = 0
+fetch_size = 10 
+keywords = ["PASSPO", "AKB48"]
+keywords.each{ |keyword|
+	result_map = search_tweet(tw, keyword, since_id, fetch_size)
+	print_tweets(result_map)
+}
--- search_test.rb
+++ search_test.rb
@@ -13,21 +13,31 @@ tw = Twitter::Client.new(
   oauth_token_secret: config_p["oauth_token_secret"]
 )
 
+## search tweets
+def search_tweet(tw, keyword, since_id, fetch_size)
+	# tw.search(keyword, :count => fetch_size, :result_type => "recent", :since_id => since_id).results.reverse.map do |status|
+	result_map  = tw.search(keyword, :count => fetch_size, :result_type => "recent", :since_id => since_id).results.reverse.map 
+	result_map.each{|status|
+		# Tweet ID, ユーザ名、Tweet本文、投稿日を1件づつ表示
+		puts status.id
+		puts status.created_at
+		puts "@" + status.from_user
+		puts status.text
+		print("\n")
+		# 取得したTweet idをsince_idに格納
+		# ※古いものから新しい順(Tweet IDの昇順)に表示されるため、
+	 	#  最終的に、取得した結果の内の最新のTweet IDが格納され、
+		#  次はこのID以降のTweetが取得される
+		since_id=status.id
+	}
+      return since_id
+end
+
 ## データの検索
 since_id = 0
 counter = 0
-keyword = "PASSPO"
-
-tw.search(keyword, :count => 10, :result_type => "recent", :since_id => since_id).results.reverse.map do |status|
-	# Tweet ID, ユーザ名、Tweet本文、投稿日を1件づつ表示
-	puts status.id
-	puts status.created_at
-	puts "@" + status.from_user
-	puts status.text
-	print("\n")
-	# 取得したTweet idをsince_idに格納
-	# ※古いものから新しい順(Tweet IDの昇順)に表示されるため、
- 	#  最終的に、取得した結果の内の最新のTweet IDが格納され、
-	#  次はこのID以降のTweetが取得される
-	since_id=status.id
-      end
+fetch_size = 10 
+keywords = ["PASSPO", "AKB48"]
+keywords.each{ |keyword|
+	after_since_id = search_tweet(tw, keyword, since_id, fetch_size)
+}
