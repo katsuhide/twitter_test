4e507aa67d0a2aca9b822da7f1514308ea766a08
katsuhide <AirMyac@AirMyac.local>
2014-01-04 13:44:30 +0900

    update

--- mikan_search.rb
+++ mikan_search.rb
@@ -3,49 +3,51 @@ require 'twitter'
 require 'yaml'
 require 'json'
 
-## 設定の読み込み
-config = YAML.load_file("config.yml")
-config_p = config["production"]
+## read the configuration for oauth
+def read_configuration
+	config = YAML.load_file("config.yml")
+	config_p = config["production"]
+	return config_p
+end
 
-tw = Twitter::Client.new(
-  consumer_key: config_p["consumer_key"],
-  consumer_secret: config_p["consumer_secret"],
-  oauth_token: config_p["oauth_token"],
-  oauth_token_secret: config_p["oauth_token_secret"]
-)
-
-## search tweets
+## search tweets method
 def search_tweet(tw, keyword, since_id, fetch_size)
 	result_map  = tw.search(keyword, :count => fetch_size, :result_type => "recent", :since_id => since_id).results.reverse.map
       return result_map
 end
 
-## print result of the search tweets
+## print result of the search tweets method
 def print_tweets(result_map)
 	result_map.each{ |result|
-		# result  => hash
+		## result  => hash
 		h = Hash::new
 		h.store("id", result.id)
 		h.store("time", result.created_at)
 		h.store("user", "@" + result.from_user)
-		h.store("text", result.text)
-		# hash => json
-		# j = JSON.pretty_generate(h)
-		# j = JSON.generate(h)
-		#  json => text file
-		# open("tweet.json", "a") do |io|
-			# JSON.dump(j, io)
-		open("tweet2.txt", "a") do |io|
+		# h.store("text", result.text)
+		config_p = read_configuration
+		open(config_p['output_file_name'], "w") do |io|
 			io.write(h.values)
 			io.write("\n")
 		end
 	}
 end
 
-## データの検索
+## create the twiter client
+config_p = read_configuration
+tw = Twitter::Client.new(
+	 consumer_key: config_p["consumer_key"],
+	 consumer_secret: config_p["consumer_secret"],
+	 oauth_token: config_p["oauth_token"],
+	 oauth_token_secret: config_p["oauth_token_secret"]
+)
+
+## execute to search tweet
 since_id = 0
-fetch_size = 10000
-f = open("anime_list.txt")
+fetch_size = config_p['fetch_size']
+hoge = config_p['keyword_list_file_name']
+puts hoge
+f = open(config_p['keyword_list_file_name'])
 keywords = f.readlines()
 # keywords = ["PASSPO", "AKB48"]
 keywords.each{ |keyword|
